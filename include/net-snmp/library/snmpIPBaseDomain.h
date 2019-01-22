0bd682160fb62f06121b02bd57c70da25f0af160
Bill Fenner <fenner@gmail.com>
2019-01-22 12:11:22 -0800

    Introduce Linux network namespace support
    Linux network namespace support: the netsnmp_socketat()
    function allows opening a socket in a different network
    namespace by temporarily using the setns() system call
    around the socket() call.  Add support for tcp/udp v4/v6
    for an additional network namespace argument, separated
    from the optional interface name by another "@".
    
    In the tests, we use two slightly unusual mechanisms to
    ensure that we are not talking in the default namespace:
    - for IPv4, we use the class E address 240.0.0.42.  We can
      not use any address inside 127/8, since some systems assign
      all of 127/8 to lo, so there's no way to distinguish
      communicating on 127.0.0.42 in the newly-created namespace
      or in the outer namespace.
    - for IPv6, we use a loopback-like address ::42/128.
      Only ::1 is defined to be the loopback in IPv6, and
      the lo device is only assigned ::1/128, so we can
      be confident that if we are communicating using ::42,
      that it is the interface inside the namespace that
      we assigned it to.

--- include/net-snmp/library/snmpIPBaseDomain.h
+++ include/net-snmp/library/snmpIPBaseDomain.h
@@ -7,6 +7,7 @@
  * SNMP endpoint specification.
  * @a: Address family, network address and port number.
  * @iface: Network interface name in ASCII format. May be empty.
+ * @ns: Network namespace for this address. May be empty.
  */
 struct netsnmp_ep {
     union {
@@ -17,22 +18,26 @@ struct netsnmp_ep {
 #endif
     } a;
     char iface[16];
+    char ns[16];
 };
 
 /**
  * SNMP endpoint with the network name in ASCII format.
  * @addr: Network address or host name as an ASCII string.
  * @iface: Network interface, e.g. "lo".
+ * @ns: Network namespace for this address. May be empty.
  * @port: Port number. "" means that no port number has been specified. "0"
  *   means "bind to any port".
  */
 struct netsnmp_ep_str {
     char     addr[64];
     char     iface[16];
+    char     ns[16];
     char     port[6];
 };
 
 int netsnmp_parse_ep_str(struct netsnmp_ep_str *ep_str, const char *endpoint);
 int netsnmp_bindtodevice(int fd, const char *iface);
+int netsnmp_socketat(const char *ns, int domain, int type, int protocol);
 
 #endif /* _SNMPIPBASEDOMAIN_H_ */
